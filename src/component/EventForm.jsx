17ceb6d47ceeb6e14ddd293726951b2e476eb265
Ludo <holbikl@gmail.com>
2021-12-20 18:50:38 +0100

    added submit functionality - WIP

--- src/component/EventForm.jsx
+++ src/component/EventForm.jsx
@@ -1,33 +1,55 @@
 import { useState } from "react"
 
-
-
-
-
 const EventForm = () => {
 
-    const [ text, setText ] = useState("")
+    const [ itemsArray, setItemsArray ] = useState([{
+        note: 'test note',
+        type: 'drink',
+        user: 'John Doe',
+        timestamp: '',
+    }])
+
+
+    const [ note, setNote ] = useState("")
     const [ type, setType ] = useState("")
 
-    console.log(text,"text")
+    console.log(note,"note")
     console.log(type,"type")
 
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(note,'note')
+        console.log(type,'type')
+        console.log(itemsArray,'itemsData1')
+        // setItemsData({...itemsData}, note)
+        setItemsArray([...itemsArray, {
+            note: note,
+            type: type
+
+        }])
+        console.log(itemsArray,'itemsData2')
+
+    }
+
+    console.log(itemsArray,'itemsData3')
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
-                Text
-            <input type="text" value={text} onChange={e => setText(e.target.value)}/>
+                note
+            <input type="text" value={note} onChange={(e) => setNote(e.target.value)}/>
             </label>
 
-            <select value={type} onChange={e => setType(e.target.value)} id="eventType">
+            <select value={type} onChange={(e)=>setType(e.target.value)} id="eventType">
                 <option value="note">note</option>
                 <option value="coffee">coffee</option>
                 <option value="meeting">meeting</option>
                 <option value="beer">beer</option>
             </select>
 
-            <input type="submit" value="submit" />
+
+
+            <input type="submit" />
 
             
         </form>
